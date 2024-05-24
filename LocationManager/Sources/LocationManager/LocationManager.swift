770fc3acd5f34bc2e386f9b7e46019d63b36c339
jwtamuly <lumat.apps@gmail.com>
2024-05-23 21:10:19 -0400

    fix access control

--- LocationManager/Sources/LocationManager/LocationManager.swift
+++ LocationManager/Sources/LocationManager/LocationManager.swift
@@ -11,22 +11,22 @@ import CoreLocation
 import SwiftUI
 
 @available(iOS 17.0, *)
-@Observable class LocationManager: NSObject, CLLocationManagerDelegate {
+@Observable public class LocationManager: NSObject, CLLocationManagerDelegate {
     /// The current location of the device.
-    var location: CLLocation?
+    public private(set) var location: CLLocation?
     
     /// A boolean indicating whether the app is authorized to access location services.
-    var isAuthorized: Bool
+    public private(set) var isAuthorized: Bool
     
     /// The current authorization status of the app regarding location services.
-    var authorizationStatus: CLAuthorizationStatus
+    public private(set) var authorizationStatus: CLAuthorizationStatus
 
     /// The internal `CLLocationManager` instance used to request and monitor location updates.
     private let locationManager = CLLocationManager()
 
     /// Initializes a new instance of `LocationManager`.
     /// Sets up the delegate and requests authorization to use location services when the app is in use.
-    override init() {
+    public override init() {
         self.isAuthorized = locationManager.authorizationStatus == .authorizedWhenInUse || locationManager.authorizationStatus == .authorizedAlways
         self.authorizationStatus = locationManager.authorizationStatus
         super.init()
@@ -35,14 +35,14 @@ import SwiftUI
     }
 
     /// Requests permission to use location services when the app is in use.
-    func requestPermission() {
+    public func requestPermission() {
         locationManager.requestWhenInUseAuthorization()
     }
 
     /// Called when the authorization status changes.
     /// Updates the `authorizationStatus` and `isAuthorized` properties accordingly.
     /// - Parameter manager: The `CLLocationManager` instance reporting the change.
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+    public func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         authorizationStatus = manager.authorizationStatus
 
         switch manager.authorizationStatus {
@@ -60,7 +60,7 @@ import SwiftUI
     /// Updates the `location` property with the latest location data.
     /// - Parameter manager: The `CLLocationManager` instance providing the data.
     /// - Parameter locations: An array of `CLLocation` objects representing the new location data.
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         location = locations.last
     }
 }
